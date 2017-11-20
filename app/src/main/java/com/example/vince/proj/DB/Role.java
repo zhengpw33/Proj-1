e08ea1beea56caada2974d710f1b5a10f118acf6
vincentbryan <2315223212@qq.com>
2017-11-20 14:43:44 +0800

    use litepal

--- app/src/main/java/com/example/vince/proj/DB/Role.java
+++ app/src/main/java/com/example/vince/proj/DB/Role.java
@@ -1,5 +1,6 @@
 package com.example.vince.proj.DB;
 
+import org.litepal.annotation.Column;
 import org.litepal.crud.DataSupport;
 
 /**
@@ -7,17 +8,35 @@ import org.litepal.crud.DataSupport;
  */
 
 public class Role extends DataSupport {
+
+    @Column(unique = true)
     private int Id;
+
+    //图片资源
     private int ImageId;
+
+    //姓名
+    @Column(unique = true)
     private String Name;
+
+    //字
+    private String SubName;
+
+    //性别
+    private String Gender;
+
+    //生卒年
+    private String LifeTime;
+
+    //籍贯
+    private String NativePlace;
+
+    //介绍
     private String Description;
 
-    public Role(int id, int imageId, String name, String description) {
-        Id = id;
-        ImageId = imageId;
-        Name = name;
-        Description = description;
-    }
+    //所属势力
+    private String Nationality;
+
 
     public int getId() {
         return Id;
@@ -49,5 +68,45 @@ public class Role extends DataSupport {
 
     public void setDescription(String description) {
         Description = description;
+    }
+
+    public String getSubName() {
+        return SubName;
+    }
+
+    public void setSubName(String subName) {
+        SubName = subName;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getLifeTime() {
+        return LifeTime;
+    }
+
+    public void setLifeTime(String lifeTime) {
+        LifeTime = lifeTime;
+    }
+
+    public String getNativePlace() {
+        return NativePlace;
+    }
+
+    public void setNativePlace(String nativePlace) {
+        NativePlace = nativePlace;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String nationality) {
+        Nationality = nationality;
     }
 }
